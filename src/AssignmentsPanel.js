9b0f0d6400aaaf8886e95d362f540400cf1e896e
Yuichi Tanikawa <t.yuichi111@gmail.com>
2017-12-22 03:25:00 +0900

    Move AssignmentsList into AssignmentsPanel

--- src/AssignmentsPanel.js
+++ src/AssignmentsPanel.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
-import AssignmentsList from './AssignmentsList';
+import { Button, Table } from 'semantic-ui-react';
 import { CelledPanelHeader, CelledPanelSegment } from './CelledPanel';
+
+function AssignmentsListItem (props) {
+  return (
+    <Table.Row>
+      <Table.Cell>
+        {props.userName} さん
+      </Table.Cell>
+      <Table.Cell>
+        {props.roleName}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
+
+function AssignmentsList (props) {
+  if (props.items.length === 0) {
+    return null;
+  }
+
+  const listItems = props.items.map((item, index) => (
+    <AssignmentsListItem key={item.id} userName={item.value} roleName={item.assignment ? item.assignment.value : ''} />
+  ));
+  return (
+    <Table attached celled striped size='large'>
+      <Table.Body>
+        {listItems}
+      </Table.Body>
+    </Table>
+  );
+}
 
 function AssignmentsPanel (props) {
   return (
