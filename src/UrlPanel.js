009aee6a7366a6466025a074158f86820f20f356
Yuichi Tanikawa <t.yuichi111@gmail.com>
2017-12-22 05:24:42 +0900

    Don't use nested shorthand

--- src/UrlPanel.js
+++ src/UrlPanel.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { Header, Input } from 'semantic-ui-react';
+import { Button, Header, Input } from 'semantic-ui-react';
 import copy from 'copy-to-clipboard';
 
 function UrlPanel (props) {
   return (
     <React.Fragment>
       <Header as='h2' content='現在の設定のURL' dividing size='small' icon='linkify' />
-      <Input
-        action={{
-          color: 'teal',
-          content: 'コピー',
-          icon: 'copy',
-          labelPosition: 'right',
-          onClick: e => copy(props.value)
-        }}
-        fluid
-        value={props.value}
-        onClick={e => e.target.select()}
-      />
+      <Input action fluid value={props.value} onClick={e => e.target.select()}>
+        <input />
+        <Button color='teal' content='コピー' icon='copy' labelPosition='right' onClick={e => copy(props.value)} />
+      </Input>
     </React.Fragment>
   );
 }
