5506a5d4d340e75d2f835b469a68c778b8154831
Yuichi Tanikawa <t.yuichi111@gmail.com>
2017-12-22 05:06:47 +0900

    Trim and filter empty params

--- src/url.js
+++ src/url.js
@@ -1,14 +1,23 @@
+function normalizeValues (values) {
+  return values.map(value => value.trim()).filter(value => value.length > 0);
+}
+
 function decodeParams (location, keys) {
   const params = new URLSearchParams(location.search);
   const out = {};
   for (const key of keys) {
-    out[key + 's'] = params.getAll(key);
+    out[key + 's'] = normalizeValues(params.getAll(key));
   }
   return out;
 }
 
-function encodeParams (location, init) {
-  const params = new URLSearchParams(init);
+function normalizeValuePairs (pairs) {
+  pairs.forEach(pair => pair[1] = pair[1].trim());
+  return pairs.filter(pair => pair[1].length > 0);
+}
+
+function encodeParams (location, valuePairs) {
+  const params = new URLSearchParams(normalizeValuePairs(valuePairs));
   const url = new URL('?' + params.toString(), location.origin);
   return url.toString();
 }
