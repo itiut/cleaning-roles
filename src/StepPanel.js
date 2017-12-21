066c6a76676523228e71a57143643c7bcb7b0b65
Yuichi Tanikawa <t.yuichi111@gmail.com>
2017-12-22 05:43:08 +0900

    Use shorthands

--- src/StepPanel.js
+++ src/StepPanel.js
@@ -4,21 +4,9 @@ import { Step } from 'semantic-ui-react';
 function StepPanel (props) {
   return (
     <Step.Group ordered size='large' widths={3}>
-      <Step>
-        <Step.Content>
-          <Step.Title>掃除する人にチェック</Step.Title>
-        </Step.Content>
-      </Step>
-      <Step>
-        <Step.Content>
-          <Step.Title>役割にチェック</Step.Title>
-        </Step.Content>
-      </Step>
-      <Step>
-        <Step.Content>
-          <Step.Title>割り当てを実行する</Step.Title>
-        </Step.Content>
-      </Step>
+      <Step title='掃除する人にチェック' />
+      <Step title='役割にチェック' />
+      <Step title='割り当てを実行する' />
     </Step.Group>
   );
 }
