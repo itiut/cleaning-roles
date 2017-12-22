2e05d1fae64b954ddab41a73f8cd4156625be192
Yuichi Tanikawa <t.yuichi111@gmail.com>
2017-12-22 20:09:55 +0900

    設定 -> データ

--- src/UrlPanel.js
+++ src/UrlPanel.js
@@ -5,7 +5,7 @@ import copy from 'copy-to-clipboard';
 function UrlPanel (props) {
   return (
     <React.Fragment>
-      <Header as='h2' content='現在の設定のURL' dividing size='small' icon='linkify' />
+      <Header as='h2' content='現在のデータのURL' dividing size='small' icon='linkify' />
       <Input action fluid value={props.value} onClick={e => e.target.select()}>
         <input />
         <Button color='teal' content='コピー' icon='copy' labelPosition='right' onClick={e => copy(props.value)} />
